e7baa7b655a1d42dcdde27e444081d8c03f44742
Максим <kasyak999@yandex.ru>
2024-09-23 23:47:15 +0300

    up

--- translator.py
+++ translator.py
@@ -3,84 +3,66 @@ import httpcore
 from tkinter import messagebox
 from googletrans import Translator
 import os
+from tkinter import ttk
 
 
-# translator = Translator()
-# translation = translator.translate("как дела?", dest="ru")
-# print(f"{translation.origin} ({translation.src}) --> {translation.text} ({translation.dest})")
-
-# BYFER = os.system('xsel -o')  # выполнить команду
-BYFER = os.popen('xsel -o').read()  # получаем значение в переменую
+BYFER = os.popen('xsel -o').read()  # Получаем значение из буфера обмена
 WIDTH = 50
 HEIGHT = 8
+TITLE = 'Переводчик v1.2'
 
-root_tk = tk.Tk()
-root_tk.configure(bg="black")  # Светло-серый фон
-# root.geometry("700x500")  # Ширина 300 пикселей, высота 200 пикселей
-root_tk.option_add("*Font", ("Arial", 20))
-root_tk.option_add("*foreground", "white")
-root_tk.option_add("*background", "black")
-root_tk.title("Переводчик v1.2")
-
-# Привязываем клавишу Esc к функции закрытия
-root_tk.bind('<Escape>', lambda event: close_app())
-root_tk.resizable(False, False)  # Запрещаем изменение размера окна
-# Создаем рамку для всего приложения
-root = tk.Frame(root_tk, borderwidth=10, relief=tk.RAISED)
-root.pack(padx=0, pady=0, fill=tk.BOTH, expand=True)
+# Инициализация переводчика
+translator = Translator()
 
 
-def click_button_ru(text_1, text_2):
-    """Перевод на русский"""
-    text_1 = text_1.get("1.0", tk.END)
-    translator = Translator()
-    translation = translator.translate(text_1, dest="ru")
+def click_translation(language, text_1, text_2):
+    """Перевод текста"""
+    text_2.config(state="normal")
+    text_input = text_1.get("1.0", tk.END).strip()
 
-    with open('perevod.txt', 'w') as f:
-        f.write(translation.text)
-    with open('perevod.txt', 'r') as f:
-        text_content = f.read()
+    if text_input:
+        try:
+            translation = translator.translate(text_input, dest=language)
+        except httpcore._exceptions.ConnectError as e:
+            error = f'Ошибка подключения к интернету.\n {e}'
+            root.withdraw()  # Скрываем основное окно
+            messagebox.showerror(TITLE, error)
+            root.destroy()
+            raise ConnectionError(error) from e
         text_2.delete("1.0", tk.END)  # Очищаем содержимое текстового поля
-        text_2.insert(tk.END, text_content)  # Вставляем текст из файла
-        os.remove("perevod.txt")
+        text_2.insert(tk.END, translation.text)  # Вставляем переведенный текст
+    text_2.config(state="disabled")
 
 
-def click_button_en(text_1, text_2):
-    """Перевод на английский"""
-    text_1 = text_1.get("1.0", tk.END)
-    translator = Translator()
-    translation = translator.translate(text_1, dest="en")
-    with open('perevod.txt', 'w') as f:
-        f.write(translation.text)
-    with open('perevod.txt', 'r') as f:
-        text_content = f.read()
-        text_2.delete("1.0", tk.END)  # Очищаем содержимое текстового поля
-        text_2.insert(tk.END, text_content)  # Вставляем текст из файла
-        os.remove("perevod.txt")
-
-
-def click_button_byfer(text_1):
+def click_button_byfer(text_1, text_2):
+    """Вставить текст из буфера обмена"""
+    text_2.config(state="normal")
     byfer = text_1.clipboard_get()  # Получаем текст из буфера обмена
     text_1.delete("1.0", tk.END)  # Очищаем содержимое текстового поля
-    text_1.insert(tk.END, byfer)  # Вставляем текст из файла
+    text_1.insert(tk.END, byfer)  # Вставляем текст из буфера
+    text_2.config(state="disabled")
 
 
 def click_button_clear(text_1, text_2):
     """Очистить поле"""
+    text_2.config(state="normal")
     text_1.delete("1.0", tk.END)
     text_2.delete("1.0", tk.END)
+    text_2.config(state="disabled")
 
 
-def close_app(event=None):
+def close_app():
     """Закрыть программу"""
-    root_tk.destroy()
+    root.destroy()
 
 
-def show_text():
-    text = 'Esc - Закрыть приложение.\n'
+def help_text():
+    """Горячии клавиши"""
+    text = 'Esc - Закрыть приложение\n'
     text += 'Ctrl+c - Копировать\n'
-    text += 'Ctrl+v - Вставить.\n'
-    info_window = tk.Toplevel(root_tk, borderwidth=10, relief=tk.RAISED)
+    text += 'Ctrl+v - Вставить\n'
+    text += 'Enter - Переместить курсор на поле ввода текста\n'
+    info_window = tk.Toplevel(root, borderwidth=10, relief=tk.RAISED)
     info_window.title("Помощь")
 
     info_window.resizable(False, False)
@@ -91,21 +73,38 @@ def show_text():
     close_button.pack(pady=20)
 
 
-def main():
+def on_key_press(event, text_1):
+    """Обработка нажатия клавиш"""
+    if event.keysym == 'Return':  # Проверяем, нажата ли клавиша Enter
+        text_1.focus_set()  # Устанавливаем фокус на text_1
+        return "break"  # Игнорируем нажатие Enter
+    if event.keysym == 'Escape':
+        close_app()
+
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.configure(bg="black")
+    root.option_add("*Font", ("Arial", 20))
+    root.option_add("*foreground", "whitesmoke")  # Цвет текста
+    root.option_add("*background", "black")
+    root.title(TITLE)
+    root.resizable(False, False)  # Запрещаем изменение размера окна
+
     # --- frame_1 -------------------------------------
     frame_1 = tk.Frame(root)
     label_1 = tk.Label(frame_1, text="Текст: ", justify=tk.LEFT)
     text_1 = tk.Text(
         frame_1, wrap=tk.WORD, width=WIDTH, height=HEIGHT,
         bg='darkslategrey')
-    scrollbar_1 = tk.Scrollbar(frame_1, command=text_1.yview)  # Создаем полосу прокрутки
-    text_1.config(yscrollcommand=scrollbar_1.set)  # Связываем текстовое поле с полосой прокрутки
+    scrollbar_1 = tk.Scrollbar(frame_1, command=text_1.yview)
+    text_1.config(yscrollcommand=scrollbar_1.set)
     text_1.insert(tk.END, BYFER)
-    text_1.config(insertbackground='white')  # цвет курсора
+    text_1.config(insertbackground='white')
 
     label_1.pack(side=tk.LEFT, padx=20)
-    text_1.pack(side=tk.RIGHT)  # Размещаем Text слева
-    scrollbar_1.pack(side=tk.RIGHT, fill=tk.Y)  # Размещаем Scrollbar справа
+    text_1.pack(side=tk.RIGHT)
+    scrollbar_1.pack(side=tk.RIGHT, fill=tk.Y)
     # --- / frame_1 -----------------------------------
 
     # --- frame_2 -------------------------------------
@@ -114,66 +113,59 @@ def main():
     text_2 = tk.Text(
         frame_2, wrap=tk.WORD, width=WIDTH, height=HEIGHT,
         bg='darkslategrey')
-    scrollbar_2 = tk.Scrollbar(frame_2, command=text_2.yview)  # Создаем полосу прокрутки
+    scrollbar_2 = tk.Scrollbar(frame_2, command=text_2.yview)
     text_2.config(yscrollcommand=scrollbar_2.set)
-    text_2.config(insertbackground='white')  # цвет курсора
+    text_2.config(insertbackground='white')
+    text_2.config(state="disabled")
 
     label_2.pack(side=tk.LEFT, padx=20)
     text_2.pack(side=tk.RIGHT)
-    scrollbar_2.pack(side=tk.RIGHT, fill=tk.Y)  # Размещаем Scrollbar справа
+    scrollbar_2.pack(side=tk.RIGHT, fill=tk.Y)
     # --- / frame_2 -----------------------------------
 
     # --- frame_3 -------------------------------------
     frame_3 = tk.Frame(root)
-    button = tk.Button(
+    button_ru = tk.Button(
         frame_3, text='Перевести на русский',
-        command=lambda: click_button_ru(
-            text_1, text_2), bg="green")
+        command=lambda: click_translation('ru', text_1, text_2), bg="green")
     button_en = tk.Button(
-        frame_3, text='Перевести на english',
-        command=lambda: click_button_en(
-            text_1, text_2), bg="blue")
-    button_byfer = tk.Button(
-        frame_3, text='Вставить из буфера',
-        command=lambda: click_button_byfer(text_1), bg="gray")
-    button_close = tk.Button(
-        frame_3, text='Очистить',
-        command=lambda: click_button_clear(text_1, text_2),
-        bg="gray")
-    button.pack(fill="both", side=tk.LEFT)
-    button_byfer.pack(fill="both", side=tk.LEFT)
-    button_close.pack(fill="both", side=tk.LEFT)
-    button_en.pack(fill="both", side=tk.LEFT)
+        frame_3, text='Перевести на английский',
+        command=lambda: click_translation('en', text_1, text_2), bg="blue")
+
+    button_ru.pack(fill="both", side=tk.LEFT, expand=True)
+    button_en.pack(fill="both", side=tk.LEFT, expand=True)
     # --- / frame_3 -----------------------------------
 
     # --- frame_4 -------------------------------------
     frame_4 = tk.Frame(root)
+    button_byfer = tk.Button(
+        frame_4, text='Вставить из буфера',
+        command=lambda: click_button_byfer(text_1, text_2), bg="gray")
+    button_clear = tk.Button(
+        frame_4, text='Очистить',
+        command=lambda: click_button_clear(text_1, text_2), bg="gray")
+
+    button_byfer.pack(fill="both", side=tk.LEFT, expand=True)
+    button_clear.pack(fill="both", side=tk.LEFT, expand=True)
+    # --- / frame_4 -----------------------------------
+
+    # --- frame_5 -------------------------------------
+    frame_5 = tk.Frame(root)
     button_help = tk.Button(
-        frame_4, text='Помощь', command=lambda: show_text(),
-        bg="gray")
+        frame_5, text='Помощь', command=help_text, bg="gray")
     button_exit = tk.Button(
-        frame_4, text='Закрыть', command=root_tk.destroy,
-        bg="red")
+        frame_5, text='Закрыть', command=close_app, bg="red")
+
     button_exit.pack(fill="both", side=tk.RIGHT)
     button_help.pack(fill="both", side=tk.RIGHT)
-    # --- / frame_4 -------------------------------------
+    # --- / frame_5 -------------------------------------
 
     frame_1.pack(fill=tk.BOTH, expand=True, pady=(0, 10))
     frame_2.pack(fill=tk.BOTH, expand=True, pady=(0, 10))
     frame_3.pack(fill=tk.BOTH, expand=True, pady=(0, 10))
-    frame_4.pack(fill=tk.BOTH, expand=True)
+    frame_4.pack(fill=tk.BOTH, expand=True, pady=(0, 10))
+    frame_5.pack(fill=tk.BOTH, expand=True)
 
-    click_button_ru(text_1, text_2)
-
-
-if __name__ == '__main__':
-    try:
-        main()
-    except httpcore._exceptions.ConnectError as e:
-        error = f'Ошибка подключения к интернету:\n {e}'
-        root_tk.withdraw()  # Скрываем основное окно
-        messagebox.showerror("Ошибка", error)
-        raise ConnectionError(error) from e
-
-    # Запускаем цикл обработки событий
-    root.mainloop()
+    root.bind("<KeyPress>", lambda event: on_key_press(event, text_1))
+    click_translation('ru', text_1, text_2)  # Сразу переводить текст
+    root.mainloop()  # Запускаем цикл обработки событий
